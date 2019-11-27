19df446baf0e2fce2972158bb33eff47b546e191
Luis <blackbarth@outlook.com>
2019-11-27 10:49:01 -0300

    Comit inicial

---
+++ APIWEB/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace APIWEB.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+    }
+}
---
+++ APIWEB/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIWEB.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
