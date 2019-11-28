4a72c680e9ba534c23a1bc3200223f496362dd09
Luis <blackbarth@outlook.com>
2019-11-28 00:00:59 -0300

    Atualizacao classes

---
+++ APIWEB/Context/APPDBContext.cs
@@ -0,0 +1,15 @@
+﻿using APIWEB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIWEB.Context
+{
+    public class APPDBContext : DbContext
+    {
+        public APPDBContext(DbContextOptions<APPDBContext> options): base(options)
+        {
+
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
