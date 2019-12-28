e248801f711e7b4fffef03f56534444498f3d6fd
Luis <blackbarth@outlook.com>
2019-12-27 23:19:46 -0300

    habilitando iis em program.cs

--- APIWEB/Program.cs
+++ APIWEB/Program.cs
@@ -21,6 +21,7 @@ namespace APIWEB
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseIISIntegration();
                 });
     }
 }
